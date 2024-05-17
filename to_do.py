b24e1eefea5f48b54345792bb0e83b16e52e6278
Vaidehi V <62456433+varshavarada@users.noreply.github.com>
2024-05-17 22:20:56 +0530

    Update to_do.py

--- to_do.py
+++ to_do.py
@@ -1,20 +1,21 @@
 #creating command line based applications
-task=[]
+tasks=[]
 def add_task(task):
   tasks.append(task)
   print(f" Yahhh!! Task '{task}' successfully added")
-def update_task(task,index,new_task):
-  if 0<=index<len(task):
-    task[index]=new_task
-    print(f" task at index {index}  updated to '{new_index}'")
+def update_task(index,new_task):
+  if 0<=index<len(tasks):
+    tasks[index]=new_task
+    print(f" task at index {index}  updated to '{new_task}'")
   else:
     print("index is invalid")
 def display_tasks():
   print("Tasks:")
-  for index task in enumarate(tasks,start=1):
-  print(f"{index}.{task}") 
+  for index,task in enumerate(tasks,start=1):
+    print(f"{index}.{task}") 
 def remove_task(task):
   tasks.remove(task)
+  print(f"Task '{task}' is removed successfully")
 #usage
 add_task("complete the assingment before the deadline")
 add_task("go to temple")
@@ -23,7 +24,7 @@ add_task("buy veggies")
 display_tasks()
 update_task(1,"finish assignment")
 display_tasks()
-remove_task("complete course")
+remove_task("complete the course")
 display_tasks()
 
 
